05e10838f495b2e82be7caa23a5adcaf5145465d
lindeberg <leon.lindeberg@hotmail.com>
2019-08-30 22:47:06 +0200

    init solution

---
+++ Dodger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dodger
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            InitWindowsForms();
+        }
+
+        private static void InitWindowsForms()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
